1f9e8b7cd187f74989fc7597c7a7b131a5386c74
Allef Sousa <allefsousa_1@hotmail.com>
2019-10-18 07:50:43 -0300

    Inicio 100 days

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+//String
+var str = "Hello, playground"
+let name = "Pedro"
+str = "Allef"
+
+//Integer
+var age = 25
+var population = 8_0_0
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var output = "My Age is \(age)"
